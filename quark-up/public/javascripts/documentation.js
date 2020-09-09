0ad5a28c22c7fb2079696dd9e81cb54ac8a05ba1
Ivam <ivam.developer@gmail.com>
2020-09-08 22:23:19 -0300

    New_structure

---
+++ quark-up/public/javascripts/documentation.js
@@ -0,0 +1,21 @@
+// Documentation file
+
+// Imports
+
+// Imports Modules
+
+// Imports components
+
+// Variables
+
+// Constant
+const doc = `Documentation`;
+
+const documentation = { 
+    doc: doc
+};
+
+// Functions
+
+// Exports
+module.exports.documentation = documentation;
---
+++ quark-up/public/javascripts/home.js
@@ -0,0 +1,26 @@
+// Home file
+
+// Imports
+
+// Imports Modules
+
+// Imports components
+
+// Variables
+
+// Constant
+const home = `<div class="container">
+                <i class="fas fa-graduation-cap fa-7x"></i>
+                <h2>Grad Core js</h2>
+                <h3>Framework for learning Objects</h3>
+                <p>Version 1.1.0</p>
+            </div>`;
+
+const homepage = { 
+    home: home
+};
+
+// Functions
+
+// Exports
+module.exports.homepage = homepage;
